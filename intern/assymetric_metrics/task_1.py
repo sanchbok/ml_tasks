a89732e64324fc64f97b17045413788ed260e907
Gamzat <gmurtazaaliev@gmail.com>
2023-03-05 14:07:37 +0300

    add cold_start solution

---
+++ intern/assymetric_metrics/task_1.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def turnover_error(y_true: np.array, y_pred: np.array) -> float:
+    pass
+    return error
--- intern/cold_start/task.py
+++ intern/cold_start/task.py
@@ -4,5 +4,11 @@ import pandas as pd
 def fillna_with_mean(
     df: pd.DataFrame, target: str, group: str
 ) -> pd.DataFrame:
-    ...
-    return df
+    group_means = df.groupby(group)[target].mean().reset_index()
+    df_without_na = df.copy()
+
+    df_without_na[target] = df[target].combine_first(
+        df[[group, target]].merge(group_means, how='left',
+                                  on=group, suffixes=('', '_new'))[f'{target}_new'].round()
+    )
+    return df_without_na
