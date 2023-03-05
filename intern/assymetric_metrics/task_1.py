1bdcd3c639f01e01f0acde3f9d28d4e18d0fbff2
Gamzat <gmurtazaaliev@gmail.com>
2023-03-05 22:50:25 +0300

    fix assymetric_metrics solutions

--- intern/assymetric_metrics/task_1.py
+++ intern/assymetric_metrics/task_1.py
@@ -2,4 +2,5 @@ import numpy as np
 
 
 def turnover_error(y_true: np.array, y_pred: np.array) -> float:
+    ''' Metrics penalizing more for underprediction '''
     return np.mean(((y_true - y_pred) / y_pred)**2)
--- intern/assymetric_metrics/task_2.py
+++ intern/assymetric_metrics/task_2.py
@@ -2,4 +2,5 @@ import numpy as np
 
 
 def ltv_error(y_true: np.array, y_pred: np.array) -> float:
+    ''' Metrics penalizing more for overestimation '''
     return np.mean(((y_true - y_pred) * (y_pred + 1))**2)
